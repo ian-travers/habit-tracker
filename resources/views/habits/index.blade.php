1619b04759b30c076562856af9bf702948818c13
Ian Travers <mr.itpe@gmail.com>
2022-11-17 16:36:25 +0300

    Habits UI Scaffold (VI) [svg circle tricks using AlpineJS]

--- resources/views/habits/index.blade.php
+++ resources/views/habits/index.blade.php
@@ -19,6 +19,31 @@
                                 + 1
                             </button>
                         </div>
+                        <div x-data="{ percent: 30, circumference: 30 * 2 * Math.PI }" class="flex basis-1/4 justify-center">
+                            <div class="relative inline-flex items-center justify-center overflow-hidden">
+                                <div
+                                    x-text="`${percent}%`" 
+                                    class="flex absolute inset-0 items-center justify-center text-sm font-semibold"
+                                ></div>
+                                <svg class="w-20 h-20 transform -rotate-90">
+                                    <circle 
+                                    class="text-gray-300" 
+                                    stroke-width="6" 
+                                    stroke="currentColor" 
+                                    fill="transparent" 
+                                    r="30" cx="40" cy="40" />
+                                    <circle 
+                                    class="text-primary-600" 
+                                    stroke-width="6" 
+                                    stroke="currentColor" 
+                                    fill="transparent" r="30" cx="40" cy="40" 
+                                    :stroke-dasharray="circumference" 
+                                    :stroke-dashoffset="circumference - (percent / 100 * circumference)"
+                                    stroke-linecap="round" 
+                                />
+                                </svg>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
