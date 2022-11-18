103c56d3a094f09dfcad52ecd77924faa3079b4f
Ian Travers <mr.itpe@gmail.com>
2022-11-18 10:29:56 +0300

    Finish test for habit description

--- tests/Unit/HabitInfo.test.js
+++ tests/Unit/HabitInfo.test.js
@@ -1,15 +1,25 @@
-import { describe, it, expect } from "vitest";
+import { describe, it, expect, beforeEach } from "vitest";
 import { mount } from "@vue/test-utils";
 import HabitInfo from "@/components/HabitInfo.vue";
 
 describe("HabitInfi.vue", () => {
-    it("displays the habit name", () => {
-        const wrapper = mount(HabitInfo, {
+    let wrapper = null;
+
+    beforeEach(() => {
+        wrapper = mount(HabitInfo, {
             props: {
                 name: "Drink water",
+                times_per_day: 3,
+                executions_count: 1,
             },
         });
+    });
 
+    it("displays the habit name", () => {
         expect(wrapper.find("#name").text()).toBe("Drink water");
+    });
+
+    it("displays the executions", () => {
+        expect(wrapper.find("#executions").text()).toBe("1 / 3 times");
     });
 });
