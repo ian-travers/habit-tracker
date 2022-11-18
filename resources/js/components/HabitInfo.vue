67661451a5166b0b8e1246265269bd8bf731aa71
Ian Travers <mr.itpe@gmail.com>
2022-11-18 13:35:17 +0300

    Getting three components together with Vue app

--- resources/js/components/HabitInfo.vue
+++ resources/js/components/HabitInfo.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex basis-1/2 flex-col">
         <h1 id="name" class="text-base font-semibold">{{ name }}</h1>
-        <small id="executions" class="font-normal text-gray-400">{{ executions_count }} / {{ times_per_day }} times</small>
+        <small id="executions" class="font-normal text-gray-500">{{ executions_count }} / {{ times_per_day }} times</small>
     </div>
 </template>
 
--- resources/views/habits/index.blade.php
+++ resources/views/habits/index.blade.php
@@ -1,49 +1,17 @@
 <x-app-layout>
-    <div class="flex flex-col bg-gray-200 min-h-screen justify-center">
+    <div id="app" class="flex flex-col bg-gray-200 min-h-screen justify-center">
         <div class="flex flex-col bg-white pt-10 pb-8 sm:max-w-lg sm:min-w-[500px] sm:mx-auto px-6 sm:px-10 shadow-xl ring-1 ring-gray-900/10 sm:rounded-xl">
             <div class="flex items-center justify-between">
-                <button type="button" class="text-white bg-primar
                 <h1 class="text-xl font-semibold">Habit Tracker</h1>
                 <button type="button" class="text-white bg-primary-600 px-3.5 py-2 rounded-md">New Habit</button>
             </div>
 
-            <div class="divide-y divide-gray-300/10">
+           <div class="divide-y divide-gray-300/10 mt-6">
                 <div class="text-base leading-7 text-gray-900">
                     <div class="flex items-center py-2.5">
-                        <div class="flex basis-1/2 flex-col">
-                            <h1 class="text-base font-semibold">Drink water</h1>
-                            <small class="font-normal text-gray-400">1 / 3 times</small>
-                        </div>
-                        <div class="flex basis-1/4 justify-center p-2.5">
-                            <button class="text-white bg-primary-600 font-semibold text-2xl px-4 py-2 rounded-lg">
-                                + 1
-                            </button>
-                        </div>
-                        <div x-data="{ percent: 30, circumference: 30 * 2 * Math.PI }" class="flex basis-1/4 justify-center">
-                            <div class="relative inline-flex items-center justify-center overflow-hidden">
-                                <div
-                                    x-text="`${percent}%`" 
-                                    class="flex absolute inset-0 items-center justify-center text-sm font-semibold"
-                                ></div>
-                                <svg class="w-20 h-20 transform -rotate-90">
-                                    <circle 
-                                    class="text-gray-300" 
-                                    stroke-width="6" 
-                                    stroke="currentColor" 
-                                    fill="transparent" 
-                                    r="30" cx="40" cy="40" />
-                                    <circle 
-                                    class="text-primary-600" 
-                                    stroke-width="6" 
-                                    stroke="currentColor" 
-                                    fill="transparent" r="30" cx="40" cy="40" 
-                                    :stroke-dasharray="circumference" 
-                                    :stroke-dashoffset="circumference - (percent / 100 * circumference)"
-                                    stroke-linecap="round" 
-                                />
-                                </svg>
-                            </div>
-                        </div>
+                        <habit-info name="Drink waterloo" times_per_day="3" executions_count="1"></habit-info>
+                        <execute-button></execute-button>
+                        <progress-bar percent="30"></progress-bar>
                     </div>
                 </div>
             </div>
